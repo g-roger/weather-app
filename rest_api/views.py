d212ffa51cd529dc953543df13401ccb35593666
Gabriel Roger <gabriel.nroger@gmail.com>
2021-07-23 19:52:29 -0300

    Create get weather behavior

--- rest_api/views.py
+++ rest_api/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from rest_api.models import Weather
 from rest_api.serializers import WeatherSerializer
 
 
@@ -13,3 +14,28 @@ class WeatherViewSet(APIView):
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        sort = request.query_params.get('sort', None)
+        city = request.query_params.getlist('city', None)
+        date = request.query_params.get('date', None)
+
+        query = Weather.objects
+
+        if date:
+            query = query.filter(date=date)
+
+        if city:
+            query = query.filter(city__iregex=r'(' + '|'.join(city) + ')')
+
+        if sort:
+            if sort == '-date':
+                sort = '-date'
+            else:
+                sort = 'date'
+            query = query.order_by(sort, 'id')
+        else:
+            query = query.order_by('id')
+
+        serializer = WeatherSerializer(query.all(), many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
