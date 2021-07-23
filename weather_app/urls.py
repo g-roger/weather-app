0e39aed2115c86ac8e10ec33285e386293c9b0df
Gabriel Roger <gabriel.nroger@gmail.com>
2021-07-23 18:46:55 -0300

    Add post weather endpoint on urls

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,3 +1,7 @@
-# Wire up our API here
+from django.urls import path
+from rest_api.views import WeatherViewSet
 
-urlpatterns = []
+
+urlpatterns = [
+    path('weather/', WeatherViewSet.as_view())
+]
