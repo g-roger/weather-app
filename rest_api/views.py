75507c518e5177f9bf84169abf8cfb3df9769e24
Gabriel Roger <gabriel.nroger@gmail.com>
2021-07-23 18:46:26 -0300

    Add weather view post method

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,1 +1,15 @@
-# implement your views here
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from rest_api.serializers import WeatherSerializer
+
+
+class WeatherViewSet(APIView):
+    def post(self, request):
+        serializer = WeatherSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
