83e7553a515d046d65f9db208c8e7ef5c3b51fc2
Gabriel Roger <gabriel.nroger@gmail.com>
2021-07-23 18:45:43 -0300

    Add weather model

--- rest_api/models.py
+++ rest_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Implement your models here
+
+class Weather(models.Model):
+    date = models.DateField()
+    lat = models.DecimalField(decimal_places=4, max_digits=12)
+    lon = models.DecimalField(decimal_places=4, max_digits=13)
+    city = models.CharField(max_length=120)
+    state = models.CharField(max_length=120)
